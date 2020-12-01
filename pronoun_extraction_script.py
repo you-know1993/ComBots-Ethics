484d05bec35bf89a6f576619c50a7bb27833f18b
you-know1993 <35299180+you-know1993@users.noreply.github.com>
2020-12-01 20:48:29 +0100

    Added visual component

--- pronoun_extraction_script.py
+++ pronoun_extraction_script.py
@@ -1,21 +1,60 @@
 # imports
 import requests
+import numpy as np
+import cv2
 NAMEAPI_KEY="c5daf3adac2a3e85791630c643d55611-user1"
 url = ("http://api.nameapi.org/rest/v5.3/genderizer/persongenderizer?"
     f"apiKey={NAMEAPI_KEY}"
 )
 
+FACE_THRESHOLD_MALE=0.35
+FACE_THRESHOLD_FEMALE=0.65
+from cltl_face_all.face_alignment import FaceDetection
+from cltl_face_all.arcface import ArcFace
+from cltl_face_all.agegender import AgeGender
+
+ag = AgeGender(device='cpu')
+af = ArcFace(device='cpu')
+fd = FaceDetection(device='cpu', face_detector='blazeface')
+
 def get_visual_gender(image_filepath):
     """
     Reads image
     Load Tae's gender-age code. https://github.com/leolani/cltl-face-all
-    Extract gender & confidence.
-    Translate gender and confidence to Male, Female, Unknown/Neutral (0,1,2)
-    # Maybe we need a function to read the image file path to the format needed for Tae's module
+    Extract gender
+    Translate gender  to Male, Female, Unknown/Neutral (0,1,2)
+    Maybe we need a function to read the image file path to the format needed for Tae's module
+
+    NOTE: THRESHOLDS ARE ASSUMED AND UNTESTED!
 
     :param image_filepath: string to filepath
     :return: 0,1,2 for male, female, unknown
     """
+    img_BGR = cv2.imread(image_filepath)
+    img = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2RGB)
+
+    bboxes = fd.detect_faces(img[np.newaxis, ...])
+    landmarks = fd.detect_landmarks(img[np.newaxis, ...], bboxes)
+    faces = fd.crop_and_align(img[np.newaxis, ...], bboxes, landmarks)
+
+    # There is only one image per batch. fd returns a list
+    bbox = bboxes[0]
+    landmark = landmarks[0]
+    face = faces[0]
+    if len(bbox) > 0:
+        #print(f"number of faces in this frame: {len(bbox)}")
+
+        # ag and af return a np.ndarray
+        _, predicted_gender = ag.predict(face)
+        print(predicted_gender)
+        if predicted_gender < FACE_THRESHOLD_MALE:
+            return 0 #for male
+        elif predicted_gender > FACE_THRESHOLD_FEMALE:
+            return 1 #for female
+        else:
+            return 2 #fer unknown
+
+
 
 def get_name_gender(name_string):
     """
@@ -35,7 +74,7 @@ def get_name_gender(name_string):
             "personName": {
                 "nameFields": [
                     {
-                        "string": f"{name}",
+                        "string": f"{name_string}",
                         "fieldType": "GIVENNAME"
                     }]
             },
@@ -54,15 +93,16 @@ def get_name_gender(name_string):
         resp.raise_for_status()
         # Decode JSON response into a Python dict:
         resp_dict = resp.json()
+        print(resp_dict)
         name_gender = resp_dict['gender']
         if name_gender == 'MALE':
-            name_gender_int = 1
+            name_gender_int = 0
         elif name_gender == 'FEMALE':
-            name_gender_int = 2
+            name_gender_int = 1
         elif name_gender == 'NEUTRAL':
-            name_gender_int= 3
+            name_gender_int= 2
         elif name_gender == 'UNKNOWN':
-            name_gender_int = 3
+            name_gender_int = 2
         return name_gender_int
     except requests.exceptions.HTTPError as e:
         print("Bad HTTP status code:", e)
@@ -81,15 +121,15 @@ def greeting_script():
 
     print("Hi! I'm Leolani, my pronouns are she/her. I love getting to know new people.")
     print("While I am learning, please fill in your name and pronouns in the following format: 'name, pro/nouns' or just 'name'")
-    answer = input("Who are you?")
+    answer = input("Who are you? \n")
 
     if "," in answer:
         name, pronouns = answer.split(", ")
-        print(f"Nice to meet you {name}. Your pronouns are {pronouns}")
+        print(f"Nice to meet you, {name}. Your pronouns are {pronouns}")
     else:
         name = answer
         # pronouns remain unchanged (None)
-        print(f"Nice to meet you {name}.")
+        print(f"Nice to meet you, {name}.")
         # TODO: remove later
         print("You have not specified your pronouns.")
 
@@ -103,13 +143,13 @@ def suggest_pronouns_script(suggesting_pronouns):
     """
     pronouns = None
 
-    pronoun_ok = input(f"Would you like me to refer to you as {suggesting_pronouns}? y/n")
+    pronoun_ok = input(f"Would you like me to refer to you as {suggesting_pronouns}? y/n \n")
 
     if pronoun_ok == "y":
         pronouns = suggesting_pronouns
     else:
         print("My apologies, I am still learning about the human world.")
-        pronouns = input("Which pronouns would you like me to use to refer to you?")
+        pronouns = input("Which pronouns would you like me to use to refer to you? \n")
 
     return pronouns
 
@@ -129,7 +169,7 @@ def pronoun_retrieving_script(name_gender, visual_gender):
         # TODO remove first print statement
         print("I cannot detect gender based on your name.")
 
-        pronouns = input("Which pronouns would you like me to use to refer to you?")
+        pronouns = input("Which pronouns would you like me to use to refer to you? ")
 
     # If gender of name and visual match, assume pronouns
     elif name_gender == visual_gender:
@@ -169,6 +209,10 @@ def create_triple(name_string, pronouns_string):
     :param pronouns_string: string of pronouns
     :return: triple in Leolani brain format
     """
+    #Is this usefull?
+    return (f'LeolaniWolrd:{name_string.lower()}', 'has_pronouns', f'pronouns:{pronouns_string.lower()}')
+
+
 
 def store_triple(triple_object):
     """
@@ -178,8 +222,8 @@ def store_triple(triple_object):
     """
 
 def main_inprogress():
-    temp_visual_gender = 1
-    temp_name_gender = 0
+    #temp_visual_gender = 1
+    image_path='merel_test.jpg'
 
     # Set global variables
     name = None
@@ -187,16 +231,15 @@ def main_inprogress():
 
     # Leolani introduces herself
     name, pronouns = greeting_script()
-
     # If pronouns are not given
     if pronouns == None:
-        visual_gender = temp_visual_gender
-        name_gender = temp_name_gender
+        visual_gender = get_visual_gender(image_path)
+        name_gender = get_name_gender(name)
 
         # Run through script to extract pronouns either by asking or assuming
         pronouns = pronoun_retrieving_script(name_gender, visual_gender)
 
-    print("Name, pronouns:", name, pronouns)
+    print(create_triple(name, pronouns))
 
 if __name__ == "__main__":
     main_inprogress()
